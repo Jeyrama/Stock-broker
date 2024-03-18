8671c03f1c57e464e4b46465ee1e8ccbb9a7ba14
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-03-18 15:47:39 -0700

    Update Stock-broker.js

--- Stock-broker.js
+++ Stock-broker.js
@@ -39,3 +39,22 @@ Note:
 
 
 // Solution
+
+function balanceStatements(list){
+  let bought = 0, sold = 0, bad = [];
+  list = list.trim();
+  list = list !== '' ? list.split(',') : [];
+  for (let i in list) {
+    if (!/^\s*[^\s]+ \d+ \d*\.\d+ (B|S)\s*$/.test(list[i])) 
+      bad.push(list[i].trim());
+    else { 
+      let order = list[i].trim().split(' ');
+      if (order[3] === 'B') bought += order[1]*order[2];
+      if (order[3] === 'S') sold += order[1]*order[2];
+    }
+  }
+  return 'Buy: ' + Math.round(bought) +
+         ' Sell: ' + Math.round(sold) +
+         (bad.length === 0 ? '' :
+         '; Badly formed ' + bad.length + ': ' + bad.join(' ;') + ' ;');
+}
